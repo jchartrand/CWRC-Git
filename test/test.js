73ade77139455a1908aeb386d86eb724abab3fd2
Luciano Frizzera <lucaju@me.com>
2020-01-23 23:25:38 -0500

    style: remove white spaces, replace var for const, use single quotes

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-var cwrcGit = require("../src/index.js");
-var nock = require('nock');
-var expect = require('chai').expect;
-var config = require('../config.js');
-var fixtures = require('../fixturesAndMocks/fixtures.js');
-var mocks = require('../fixturesAndMocks/mocks.js');
-let templateMocks = require('../fixturesAndMocks/templateMocks.js')
-var request = require('request')
+const cwrcGit = require('../src/index.js');
+const nock = require('nock');
+const expect = require('chai').expect;
+const config = require('../config.js');
+const fixtures = require('../fixturesAndMocks/fixtures.js');
+const mocks = require('../fixturesAndMocks/mocks.js');
+const templateMocks = require('../fixturesAndMocks/templateMocks.js')
+const request = require('request')
 
 // uncomment this next line to use actual authentication with a real github personal token
 cwrcGit.authenticate(config.personal_oath_for_testing);
@@ -44,9 +44,9 @@ and then just proceed with tests as-is, so that no need to setup
 nocks in the beforeEach:  tape-nock will take care of it all.
  */
 
-describe("cwrcGit", function () {
+describe('cwrcGit', function () {
 
-	describe(".getDoc", function () {
+	describe('.getDoc', function () {
 
 		beforeEach(function () {
 			mocks.getDoc()
@@ -56,7 +56,7 @@ describe("cwrcGit", function () {
 
 		});
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 
 			cwrcGit.getDoc(fixtures.owner, fixtures.testRepo, 'jchartrand', 'curt/qurt/test.txt')
 				.then(result => {
@@ -71,14 +71,14 @@ describe("cwrcGit", function () {
 
 	});
 
-	describe(".getRepoContents", function () {
+	describe('.getRepoContents', function () {
 		beforeEach(function () {
 			var getRepoContentsByDrillDownBranchNock = mocks.getRepoContentsByDrillDownBranchNock()
 			var getRepoContentsNock = mocks.getRepoContentsNock();
 			var getRepoContentsTree = mocks.getRepoContentsTree()
 		})
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.getRepoContents(fixtures.owner, fixtures.testRepo)
 				.then(result => {
 					expect(result).to.exist;
@@ -89,7 +89,7 @@ describe("cwrcGit", function () {
 	})
 
 
-	describe(".getRepoContentsByDrillDown", function () {
+	describe('.getRepoContentsByDrillDown', function () {
 		beforeEach(function () {
 			var getRepoContentsByDrillDownBranchNock = mocks.getRepoContentsByDrillDownBranchNock()
 			var getRepoContentsByDrillDownRootTreeNock = mocks.getRepoContentsByDrillDownRootTreeNock()
@@ -97,7 +97,7 @@ describe("cwrcGit", function () {
 			var getReposByDrillDownThirdLevelNock = mocks.getReposByDrillDownThirdLevelNock()
 		})
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.getRepoContentsByDrillDown(fixtures.owner, fixtures.testRepo)
 				.then(result => {
 					expect(result).to.exist
@@ -108,13 +108,13 @@ describe("cwrcGit", function () {
 		})
 	})
 
-	describe(".getReposForUser", function () {
+	describe('.getReposForUser', function () {
 
 		beforeEach(function () {
 			var getReposForGithubUserNock = mocks.getReposForGithubUserNock();
 		});
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.getReposForUser(fixtures.owner, 1, 10)
 				.then(result => {
 					expect(result).to.exist;
@@ -126,31 +126,31 @@ describe("cwrcGit", function () {
 	});
 
 
-	describe(".getReposForAuthenticatedUser", function () {
+	describe('.getReposForAuthenticatedUser', function () {
 
 		beforeEach(function () {
 			var getReposForAuthenticatedUserNock = mocks.getReposForAuthenticatedUserNock();
 		});
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.getReposForAuthenticatedUser('owner', 1, 10)
 				.then(result => {
 					expect(result).to.exist;
 					expect(result.data[0].name).to.equal(fixtures.testRepo);
 					done();
 				})
-		})//.timeout(5000); // to force mocha to wait longer for async to return
+		}) //.timeout(5000); // to force mocha to wait longer for async to return
 
 	});
 
 
-	describe(".createRepo", function () {
+	describe('.createRepo', function () {
 		beforeEach(function () {
 			var createGithubRepoNock = mocks.getCreateGithubRepoNock();
 			var getMasterBranchFromGithubNock = mocks.getMasterBranchFromGithubNock();
 		})
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.createRepo(fixtures.testRepo, fixtures.testRepoDescription, fixtures.isPrivate)
 				.then(
 					result => {
@@ -162,7 +162,7 @@ describe("cwrcGit", function () {
 		}).timeout(5000);
 	})
 
-	// describe(".createBranchFromMaster", function() {
+	// describe('.createBranchFromMaster', function() {
 
 	// 	beforeEach(function () {
 	// 		mocks.createBranchFromMasterGetMaster()
@@ -170,7 +170,7 @@ describe("cwrcGit", function () {
 	// 		mocks.createBranchGeneric()
 	// 	})
 
-	// 	it("returns valid url for new ref if created", function(done){
+	// 	it('returns valid url for new ref if created', function(done){
 	// 		cwrcGit.createBranchFromMaster(
 	// 			{
 	// 				"owner": fixtures.owner,
@@ -367,16 +367,16 @@ describe("cwrcGit", function () {
 	// 	}).timeout(9000);
 	// })
 
-	describe(".saveAsPullRequest", function () {
+	describe('.saveAsPullRequest', function () {
 		beforeEach(function () {
-				mocks.getUserBranchHeadNock()
-				mocks.getLatestFileSHANock()
-				mocks.saveExistingFileNock()
-				mocks.findExistingPRNock()
-				mocks.saveNewFileNock()
+			mocks.getUserBranchHeadNock()
+			mocks.getLatestFileSHANock()
+			mocks.saveExistingFileNock()
+			mocks.findExistingPRNock()
+			mocks.saveNewFileNock()
 			mocks.getLatestFileSHANockForNew()
 		})
-		it("returns correctly for existing file", function (done) {
+		it('returns correctly for existing file', function (done) {
 			cwrcGit.saveAsPullRequest(
 					fixtures.owner, fixtures.testRepo, 'curt/qurt/testq339.txt', fixtures.testDoc,
 					'jchartrand', 'some commit message', 'glorious title for PR'
@@ -390,7 +390,7 @@ describe("cwrcGit", function () {
 					}
 				)
 		}).timeout(9000);
-		it("returns correctly for new file", function (done) {
+		it('returns correctly for new file', function (done) {
 			cwrcGit.saveAsPullRequest(
 					fixtures.owner, fixtures.testRepo, 'curt/qurt/testuufy.txt', fixtures.testDoc,
 					'jchartrand', 'some commit message', 'glorious title for PR'
@@ -406,13 +406,13 @@ describe("cwrcGit", function () {
 		}).timeout(9000);
 	})
 
-	describe(".getDetailsForAuthenticatedUser", function () {
+	describe('.getDetailsForAuthenticatedUser', function () {
 
 		beforeEach(function () {
 			var getDetailsForAuthenticatedUserNock = mocks.getDetailsForAuthenticatedUserNock();
 		});
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.getDetailsForAuthenticatedUser()
 				.then(
 					result => {
@@ -424,13 +424,13 @@ describe("cwrcGit", function () {
 
 	});
 
-	describe(".getTemplates", function () {
+	describe('.getTemplates', function () {
 
 		beforeEach(function () {
 			templateMocks();
 		});
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.getTemplates('cwrc', 'CWRC-Writer-Templates', 'master', 'templates')
 				.then(
 					result => {
@@ -442,13 +442,13 @@ describe("cwrcGit", function () {
 
 	});
 
-	describe(".searchCode", function () {
+	describe('.searchCode', function () {
 
 		beforeEach(function () {
 			var getSearchNock = mocks.getSearchNock();
 		});
 
-		it("returns correctly", function (done) {
+		it('returns correctly', function (done) {
 			cwrcGit.searchCode('cwrc-melbourne+repo:jchartrand/cleanDoc2')
 				.then(
 					result => {
@@ -456,9 +456,9 @@ describe("cwrcGit", function () {
 						done()
 					}
 				)
-		})//.timeout(5000); // to force mocha to wait longer for async to return
+		}) //.timeout(5000); // to force mocha to wait longer for async to return
 
-		it("returns highlighting", function (done) {
+		it('returns highlighting', function (done) {
 			cwrcGit.searchCode('cwrc-melbourne+repo:jchartrand/cleanDoc2')
 				.then(
 					result => {
