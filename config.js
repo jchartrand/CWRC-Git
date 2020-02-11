4984b809ae9e9299f53435d4a0e1ea0fe7422825
Luciano Frizzera <lucaju@me.com>
2020-02-11 18:48:03 -0500

    chore: replace config.js for config.json

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'jwt_secret': 'someSecret',
-    'github_client_secret': '7728f6164902ecc23337beabffc7e65cc2e849ef',
-    'github_client_id': 'f1fc73ef75ab2d6acd77',
-    'github_oath_callback': 'https://localhost/github/callback',
-    'personal_oath_for_testing': '7ad12d960bbf38864c93eb2d10730fc2c598a72d'
-};
